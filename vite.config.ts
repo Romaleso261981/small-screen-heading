95d132142d33ac27a13a12e4846263e0e99622e2
Roman_Leso <ladiginscormag@gmail.com>
2025-10-04 14:58:34 +0300

    feat: configure server allowed hosts in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ["8x45t9-5173.csb.app", "localhost", "127.0.0.1"],
+  },
 });
